eba2b013ca7eed97c065a5fa20029a19d96292a5
kamilhajj <you@example.com>
2023-08-22 12:04:05 +0300

    the automation for the log in is ready for code review

---
+++ cypress/support/step_definitions/login.constants.ts
@@ -0,0 +1,4 @@
+export const loginPage = 'http://mms-filament.test/admin/login';
+export const emailField = 'input[id="email"]';
+export const passwordField = 'input[id="password"]';
+export const signInButton = 'Sign in';
