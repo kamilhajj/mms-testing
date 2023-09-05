96474ce5edd326da5cb912ddcaa9201fd1717d29
kamilhajj <you@example.com>
2023-09-05 14:46:15 +0300

    changes for the login automation

--- cypress/support/step_definitions/login/globalLogInSteps.ts
+++ cypress/support/step_definitions/login/globalLogInSteps.ts
@@ -1,22 +1,22 @@
 import { routes } from "./routes";
 
-export function LoginPage() {
+export function visitLoginPage() {
   cy.visit(Cypress.env('BASE_URL') + routes.login);
   cy.url().should('include', routes.login);
 }
 
-export function FillsLoginForm() {
+export function fillLoginForm() {
   cy.url().should('include', routes.fillLoginForm);
   cy.get('[dusk="filament.forms.email"]').click().type(Cypress.env('EMAIL'));
   cy.get('[dusk="filament.forms.password"]').click().type(Cypress.env('PASSWORD'));
 }
 
-export function ClicksSignInButton() {
+export function clickSignInButton() {
   cy.url().should('include', routes.clickSignin);
   cy.contains('Sign in').click();
 }
 
-export function AnalyticsPage() {
+export function validateAnalyticsPage() {
   cy.url().should('include', routes.validateAnalytics);
 }
 
--- cypress/support/step_definitions/login/login.ts
+++ cypress/support/step_definitions/login/login.ts
@@ -1,30 +1,26 @@
 import { When, Given, Then } from "@badeball/cypress-cucumber-preprocessor";
-import {LoginPage, FillsLoginForm, ClicksSignInButton, AnalyticsPage} from "./globalLogInSteps";
-
+import { visitLoginPage, fillLoginForm, clickSignInButton, validateAnalyticsPage } from "./globalLogInSteps";
 
 Given('the user is on the {string} page', (page) => {
-    if(page === 'Login') {
-        LoginPage();
-    }
-  });
-
+  if(page === 'Login') {
+    visitLoginPage();
+  }
+});
 
 When('the user fill the {string} form', (form) => {
-    if(form === 'loginForm') {
-        FillsLoginForm();
-    }
-  });
-
+  if(form === 'loginForm') {
+    fillLoginForm();
+  }
+});
 
 When('the user click on the {string} button', (button) => {
-    if(button === 'SignIn') {
-        ClicksSignInButton();
-    }
-  });
-
+  if(button === 'SignIn') {
+    clickSignInButton();
+  }
+});
 
 Then('the user should be redirected to the {string} page', (page) => {
-    if(page === 'Analytics') {
-        AnalyticsPage();
-    }
-  });
+  if(page === 'Analytics') {
+    validateAnalyticsPage();
+  }
+});
