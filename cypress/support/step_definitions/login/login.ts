7c0234156521fdc178a4699abec9d4406b0235ea
kamilhajj <you@example.com>
2023-09-05 12:19:14 +0300

    changes for the login automation

--- cypress/support/step_definitions/login/login.ts
+++ cypress/support/step_definitions/login/login.ts
@@ -2,10 +2,29 @@ import { When, Given, Then } from "@badeball/cypress-cucumber-preprocessor";
 import {LoginPage, FillsLoginForm, ClicksSignInButton, AnalyticsPage} from "./globalLogInSteps";
 
 
-Given('the user is on the "Login" page', LoginPage);
-    
-When('the user fill the "loginForm" form', FillsLoginForm);
+Given('the user is on the {string} page', (page) => {
+    if(page === 'Login') {
+        LoginPage();
+    }
+  });
 
-When('the user click on the "SignIn" button', ClicksSignInButton);
 
-Then('the user should be redirected to the "Analytics" page', AnalyticsPage);
+When('the user fill the {string} form', (form) => {
+    if(form === 'loginForm') {
+        FillsLoginForm();
+    }
+  });
+
+
+When('the user click on the {string} button', (button) => {
+    if(button === 'SignIn') {
+        ClicksSignInButton();
+    }
+  });
+
+
+Then('the user should be redirected to the {string} page', (page) => {
+    if(page === 'Analytics') {
+        AnalyticsPage();
+    }
+  });
